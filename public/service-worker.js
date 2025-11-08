33578c444a7a346c2d57908013ea18bb1cde61fd
Your Name <paziske@gmail.com>
2025-11-08 16:36:29 +0100

    Update PWA configuration with correct icon paths and meta tags

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,8 +5,8 @@ const urlsToCache = [
   'https://video-idea.fra1.cdn.digitaloceanspaces.com/sounds/victory-96688.mp3',
   'https://video-idea.fra1.cdn.digitaloceanspaces.com/sounds/beep-short.mp3',
   'https://video-idea.fra1.cdn.digitaloceanspaces.com/sounds/pro-only.gif',
-  '/assets/icons/icon-192x192.png',
-  '/assets/icons/icon-512x512.png',
+  '/assets/icons/android/android-launchericon-192-192.png',
+  '/assets/icons/android/android-launchericon-512-512.png',
   'https://cdn.tailwindcss.com'
 ];
 
